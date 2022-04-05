b1fcdaf88f0771dce8410ba2b8045c5751aa32e1
AledPrysLloyd <aledpryslloyd@gmail.com>
2022-04-05 22:26:14 +0100

    major

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -12,14 +12,14 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xbbbbff );
 
-		/*
+		
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
 		this.scene.add(ambient);
         
         const light = new THREE.DirectionalLight();
         light.position.set( 0.2, 1, 1);
         this.scene.add(light);
-		*/
+		
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
@@ -27,7 +27,7 @@ class App{
 		container.appendChild( this.renderer.domElement );
 		
 
-		/*
+		
         const geometry = new THREE.BoxBufferGeometry();
         const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
 
@@ -38,23 +38,23 @@ class App{
         const controls = new OrbitControls( this.camera, this.renderer.domElement );
         
         this.renderer.setAnimationLoop(this.render.bind(this));
-    	*/
+    	
         window.addEventListener('resize', this.resize.bind(this) );
 		
 	}	
     
     resize(){
-		/*
+		
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize( window.innerWidth, window.innerHeight );  
-		*/
+		
     }
     
 	render( ) {   
-		/*
+		
         this.mesh.rotateY( 0.01 );
-		*/
+		
         this.renderer.render( this.scene, this.camera );
 		
     }
