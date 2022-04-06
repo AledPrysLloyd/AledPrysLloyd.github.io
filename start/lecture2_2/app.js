87b26bae4028fa02f6af6a00d223026b52b2fa2b
AledPrysLloyd <aledpryslloyd@gmail.com>
2022-04-06 20:46:39 +0100

    test

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -45,7 +45,7 @@ class App{
         this.scene.add(this.mesh);
         */
 
-        //Add MrDwarf testing
+        //Add MrDwarf testing2
         const loader = new GLTFLoader(); 
         loader.load( '../models/MrDwarf.glb', function ( gltf ) { scene.add( gltf.scene ); }, undefined, function ( error ) { console.error( error ); } );
         
