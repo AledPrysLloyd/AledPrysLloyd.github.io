4b0965a64bce5315d1fa5518c923ee1814e0b6a2
AledPrysLloyd <aledpryslloyd@gmail.com>
2022-04-06 19:13:16 +0100

    MrDwarf

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -1,17 +1,24 @@
 import * as THREE from '../../libs/three/three.module.js';
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import { GLTFLoader } from '../../libs/three/jsm/loaders/GLTFLoader.js';
 
 class App{
 	constructor(){
+
+        //
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
         
+
+        //Camera
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
 		this.camera.position.set( 0, 0, 4 );
         
+        //Scene
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xf0f0f0 );
 
+        //Lighting
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
 		this.scene.add(ambient);
         
@@ -19,22 +26,35 @@ class App{
         light.position.set( 0.2, 1, 1);
         this.scene.add(light);
 			
+
+        //Renderer
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( this.renderer.domElement );
+
+
 		
+        //Add box
+        /*
         const geometry = new THREE.BoxBufferGeometry();
         const material = new THREE.MeshStandardMaterial( { color: 0x00FF00 });
 
         this.mesh = new THREE.Mesh( geometry, material );
         
         this.scene.add(this.mesh);
+        */
+
+        //Add dwarf
+        const loader = new GLTFLoader(); 
+        loader.load( '../models/MrDwarf.glb', function ( gltf ) { scene.add( gltf.scene ); }, undefined, function ( error ) { console.error( error ); } );
         
+
+        //Orbital Controls
         const controls = new OrbitControls( this.camera, this.renderer.domElement );
-        
         this.renderer.setAnimationLoop(this.render.bind(this));
     
+        //Window Listener
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
